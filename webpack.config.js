fe02584f75346f0b9948b82d0eb9932e00c1b4b5
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2023-05-07 15:06:12 +0530

    Setup manifest file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -19,4 +20,14 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/manifest.json'),
+          to: path.resolve('dist'),
+        },
+      ],
+    }),
+  ],
 };
